8cef043a84c2578d019b7eab90b939a990764cf4
Rodolfo Gonzalez <rodolfo.gonzalez4335@gmail.com>
2018-02-25 20:59:48 -0600

    Trying to establish client to host connection

--- Client/client.py
+++ Client/client.py
@@ -5,7 +5,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-host = "172.17.100.210"
+host = "10.145.103.233"
 server_address = (host, 10000)
 print (sys.stderr, 'connecting to %s port %s' % server_address)
 sock.connect(server_address)
