5e5c48b84d190938ebdaaf95c60fa10aed50f500
Manuel Lopez <manuellopez@wireless-10-145-175-119.public.utexas.edu>
2018-02-26 15:48:51 -0600

    idk

--- GUI/autoDemo.py
+++ GUI/autoDemo.py
@@ -6,8 +6,7 @@ from fileparsing import parsing
 import socket
 import sys
 
-def DisconnectToServer():
-    conn.close()
+
 
 class App(QWidget):
 
@@ -35,7 +34,7 @@ class App(QWidget):
         
         #Buttons Name
         self.inputButton = QPushButton("Browse", self)
-        #self.connectButton =  QPushButton("See Server Mappings", self)
+        self.connectButton =  QPushButton("Print Report", self)
         self.uploadButton = QPushButton("Upload", self)
         
         #Title placement
@@ -48,11 +47,12 @@ class App(QWidget):
         self.inputButton.move(390, 75)
         self.txtType.move(110, 105)
         self.uploadButton.move(390, 100)
+        self.connectButton.move(40, 300)
         
         #Buttons Action
         self.inputButton.clicked.connect(self.openFileNamesDialog)
         self.uploadButton.clicked.connect(self.uploadFunc)
-        #self.connectButton.clicked.connect(self.connectToServer)
+        self.connectButton.clicked.connect(self.printReport)
         
         self.setFixedSize(pixmap.width(), pixmap.height())
         
@@ -64,64 +64,73 @@ class App(QWidget):
     def sendFilesToServer(self, fileName):
         try:
             # Send data
+            print ("------------------"+"\n\n\n"+fileName)
             f = open(fileName,'rb')
             while True:
                 message = f.read(1024)
                 if not message:
+                    f.close()
                     break
-                print (sys.stderr, 'sending "%s"\n' % message)
-                sock.sendto(message,server_address)
+                print (sys.stderr, 'sending "%s"\n' % message.decode())
+                self.sock.sendto(message, self.server_address)
         except:
-            print("There was an problem sending the file data.\n")
-#        with open('datareceived.txt', 'w') as f:
-#            try:
-#                while 1:
-#                    data = sock.recv(1024)
-#                    print (sys.stderr, 'received %s' % data)
-#                    if not data:
-#                        break
-#                    f.write(data)
-#                    amount_received += len(data)
-#            except:
-#                print("There was a problem sending the file data.")
-#        f.close()
+            print("There was a problem sending the file data.\n")
 
     def openFileNamesDialog(self):
+#        self.ConnectToServer()
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         self.filenames = QFileDialog.getOpenFileNames(self,"Upload Files", "","Text files (*.txt)", options=options)
         filename, _ = self.filenames
-        self.fileNameBox.setText(str(filename))
+        if filename:
+            self.fileNameBox.setText(str(filename))
 
     def uploadFunc(self):
+        self.ConnectToServer()
         self.fileNameBox.setText("")
         f = open(str(self.filenames[0][0]), 'r')
         for i in range(0, len(self.filenames[0])):
             self.sendFilesToServer(self.filenames[0][i])
+        self.DisconnectToServer()
 
 
-    def connectToServer(self):
+    def DisconnectToServer(self):
+        self.sock.close()
+
+    def ConnectToServer(self):
+        # Create a TCP/IP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+        # Connect the socket to the port where the server is listening
+        self.server_address = ('10.145.220.247', 10000)
+        print (sys.stderr, 'connecting to %s port %s' % self.server_address)
+        self.sock.connect(self.server_address)
+
+    def printReport(self):
+        self.ConnectToServer()
         print ("Create Window")
-        #try:
-        #look for response
-        #    amount_received = 0
-        #    amount_expected = len(message)
-        #
-        #    while amount_received < amount_expected:
-        #        data = sock.recv(16)
-        #        amount_received += len(data)
-        #        print(sys.stderr, 'received %s' % message)
-        #except:
-        #    print ("There was a problem receiving the file data.")
+        # my code , creates text file containing "return"
+        r = "return"
+        self.sock.sendto(r.encode('utf-8'), server_address)
+        try:
+            f = open('datareceived.txt', 'w')
+            while 1:
+                data = self.sock.recv(1024)
+                if not data: break
+                f.write(data.decode('utf-8'))
+                f.close()
+                print (sys.stderr, 'received %s' % data)
+        except:
+            print("There was a problem receiving the file data.")
 
 if __name__ == '__main__':
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Connect the socket to the port where the server is listening
-    server_address = ('10.145.220.247', 10000)
-    print (sys.stderr, 'connecting to %s port %s' % server_address)
-    sock.connect(server_address)
+#    # Create a TCP/IP socket
+#    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+#    # Connect the socket to the port where the server is listening
+#    server_address = ('10.145.220.247', 10000)
+#    print (sys.stderr, 'connecting to %s port %s' % server_address)
+#    sock.connect(server_address)
 
     app = QApplication(sys.argv)
     ex = App()
