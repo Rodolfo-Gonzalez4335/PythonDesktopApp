22bdd6a978f63810fe16834809600239c4b6c307
Rodolfo Gonzalez <rodolfo.gonzalez4335@gmail.com>
2018-02-28 13:42:47 -0600

    Updated server it now sends messages to client

--- Server/server.py
+++ Server/server.py
@@ -4,65 +4,68 @@ import sys
 from fileparsing import fileparsing
 
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Server:
+    def __init__(self):
+        self.host = gethostbyname( '0.0.0.0' )
+        self.server_address = (self.host, 10000)
+        # Create a TCP/IP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(self.server_address)
+        self.sock.listen(1)
+        print (sys.stderr, 'starting up on %s port %s' % self.server_address)
 
-# Bind the socket to the port
-# host="127.0.0.1"
-host = gethostbyname( '0.0.0.0' )
-# host = "10.145.103.233"
-server_address = (host, 10000)
-print (sys.stderr, 'starting up on %s port %s' % server_address)
-sock.bind(server_address)
-
-# Listen for incoming connections
-sock.listen(1)
-
-while True:
-    # Wait for a connection
-    print (sys.stderr, 'waiting for a connection')
-    connection, client_address = sock.accept()
-
-    try:
+    def readCommands(self):
+        while True:
+            # Wait for a connection
+            print ('waiting for a connection')
+            connection, client_address = self.sock.accept()
             print (sys.stderr, 'connection from', client_address)
-            i=1
-            f=open('file_'+ str(i)+".txt",'w')
-            i= i+1
-            # Receive the data in small chunks and retransmit it
-            while True:
-                data = connection.recv(1024)
-                data_decoded = data.decode()
-                print (sys.stderr, 'received "%s"' % data_decoded)
-                if not data:
-                    print (sys.stderr, 'empty data from client', client_address)
-                    f.close();
-                    # idk = fileparsing("file_"+str(i))
-                    # idk.parse();
-                    break
-                if f.closed and data_decoded!="":
-                    print ("!!!!!!!!f.close"+ data_decoded)
-                    f=open('file_'+ str(i)+".txt",'w')
-                    i= i+1
-                if "EndOfFile" in data_decoded:
-                    idk = data_decoded.split("EndOfFile;")
-                    f.write(idk[0])
-                    f.close();
-                    if len(idk)==2:
-                        if idk[1] and data_decoded!="":
-                            print ("~~~~~~~~~~"+idk[1]+"~~~~~~~~~~")
+
+            try:
+                i=1
+                f=open('file_'+ str(i)+".txt",'w')
+                i= i+1
+                # Receive the data in small chunks and retransmit it
+                while True:
+                    data = connection.recv(1024)
+                    data_decoded = data.decode()
+                    if "Send Report" in data_decoded:
+                        connection.sendall("Meny is cool NOT".encode('utf-8'))
+                    else:
+                        print (sys.stderr, 'received "%s"' % data_decoded)
+                        if not data:
+                            print (sys.stderr, 'empty data from client', client_address)
+                            f.close();
+                            # idk = fileparsing("file_"+str(i))
+                            # idk.parse();
+                            break
+                        if f.closed and data_decoded!="":
+                            print ("!!!!!!!!f.close"+ data_decoded)
                             f=open('file_'+ str(i)+".txt",'w')
                             i= i+1
-                            f.write(idk[1])
-                else:
-                    f.write(data_decoded)
+                        if "EndOfFile" in data_decoded:
+                            idk = data_decoded.split("EndOfFile;")
+                            f.write(idk[0])
+                            f.close();
+                            if len(idk)==2:
+                                if idk[1] and data_decoded!="":
+                                    print ("~~~~~~~~~~"+idk[1]+"~~~~~~~~~~")
+                                    f=open('file_'+ str(i)+".txt",'w')
+                                    i= i+1
+                                    f.write(idk[1])
+                        else:
+                            f.write(data_decoded)
 
 
-                # if data:
-                    # print (sys.stderr, 'sending data back to the client')
-                    # connection.sendall(data)
-                # else:
+                        # if data:
+                            # print (sys.stderr, 'sending data back to the client')
+                            # connection.sendall(data)
+                        # else:
 
 
-    finally:
-        # Clean up the connection
-        connection.close()
+            finally:
+                # Clean up the connection
+                connection.close()
+
+server = Server();
+server.readCommands();
