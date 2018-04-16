c3dc1ad93b041869a9a655e599719df653cb2254
Manuel Lopez <manuellopez@Manuels-MacBook-Pro.local>
2018-04-16 09:56:43 -0500

    testing system

--- GUI/autoDemo.py
+++ GUI/autoDemo.py
@@ -21,6 +21,7 @@ class App(QWidget):
         self.trained = ""
         self.outputReady = ""
         self.hasCorrected = ""
+        print(sys.platform)
         if sys.platform == "darwin":
             self.initUI()
         else:
